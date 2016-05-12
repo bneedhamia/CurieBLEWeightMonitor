f32c5a8175a3787db7163bf8950e2c1364d02479
Bradford H. Needham <bneedhamia@gmail.com>
2016-05-11 23:55:37 -0700

    version 2 scale is running!  I'm in the middle of calibrating it.

--- CurieBLEWeightMonitor.ino
+++ CurieBLEWeightMonitor.ino
@@ -7,10 +7,17 @@
  * Parts:
  * - 4 load sensors
  * - 4 Load Cell Amplifier boards (HX711)
- * - 1 Arduino Uno
+ * - 1 Arduino
  */
 
 #include "HX711.h"
+
+/*
+ * CALIBRATE_SCALE = uncomment this line to report raw Load Cell Amplifier values,
+ *  for calibration.
+ * Comment out this line to run the normal scale software.
+ */
+#define CALIBRATE_SCALE 1
 
 /*
  * I/O pins:
@@ -30,14 +37,23 @@ const int PIN_HX711_UR_CLK = 6;
 const int PIN_HX711_UR_DOUT = 7;
 const int PIN_HX711_LR_CLK = 8;
 const int PIN_HX711_LR_DOUT = 9;
-//TODO Consider going with bunches of arrays of 4 elements and four const indices.
+
+//345678901234567892123456789312345678941234567895123456789612345678971234567898
+
+#ifdef CALIBRATE_SCALE
+const int SAMPLES_PER_READING = 40;   // number of samples per calibration reading.
+float values[SAMPLES_PER_READING];    // individual readings to do statistics on.
+#endif
 
 /*
- * LL_OFFSET = zero-load offset for the Lower Left Load Sensor,
- *   calculated from multiple measurements.
- * LL_SCALE = scale from Amplifier units to kg.
+ * UR_OFFSET = zero-load offset for the Upper Right Load Sensor,
+ *   calculated from CALIBRATE_SCALE with nothing, not even the top plywood circle,
+ *   on the scale.
+ * UR_SCALE = scale from Amplifier units to kg.
+ *   Calculated from CALIBRATE_SCALE and a spreadsheet. See Calculations.ods
  * 
- * LR_* = Same for Lower Right Load Sensor
+ * LR_* = Same for Lower Right Load Sensor.
+ * etc.
  */
 
 const long UR_OFFSET = 260249L;
@@ -51,7 +67,8 @@ const float UL_SCALE = 10831.330;
 
 const long LL_OFFSET = 260249L;
 const float LL_SCALE = 10831.330;
-  
+
+// *Hx711 = controller for each HX711 Load Cell Amplifier
 HX711 llHx711(PIN_HX711_LL_DOUT, PIN_HX711_LL_CLK);
 HX711 lrHx711(PIN_HX711_LR_DOUT, PIN_HX711_LR_CLK);
 HX711 ulHx711(PIN_HX711_UL_DOUT, PIN_HX711_UL_CLK);
@@ -59,10 +76,26 @@ HX711 urHx711(PIN_HX711_UR_DOUT, PIN_HX711_UR_CLK);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Raw LL, UL, UR, LR");
-  
-  // Once we've calibrated the scale, we want to load those calibrations
-  // into each Amplifier object.
+
+#ifdef CALIBRATE_SCALE
+  Serial.print(F("Calibrating, "));
+  Serial.print(SAMPLES_PER_READING);
+  Serial.println(F(" Samples per row."));
+
+  Serial.print(F("LL Average,LL Std Dev,"));
+  Serial.print(F("UL Average,UL Std Dev,"));
+  Serial.print(F("UR Average,UR Std Dev,"));
+  Serial.println(F("LR Average,LR Std Dev"));
+
+#else
+  // Set up to continuously report the scale output.
+     
+  /*
+   * Load the calibrations into each Amplifier object.
+   * For Y = MX + B, where X = weight and Y = Load Cell Value,
+   *   set_scale() sets M,
+   *   set_offset() sets B.
+   */
   
   llHx711.set_scale(LL_SCALE);
   llHx711.set_offset(LL_OFFSET);
@@ -76,28 +109,72 @@ void setup() {
   urHx711.set_scale(UR_SCALE);
   urHx711.set_offset(UR_OFFSET);
 
+#endif
+
 }
 
 void loop() {
-  float value = 0;
+
+#ifdef CALIBRATE_SCALE
 
   delay(2000);
 
-  // For now we're testing and calibrating, so just read the raw values
+  /*
+   * Read and report the average and Standard Deviation for each load sensor.
+   * For ease of pasting into the spreadsheet, print one row per set of measurements.
+    */
+    
+  reportStatistics(&llHx711);
+  Serial.print(F(","));
+  reportStatistics(&ulHx711);
+  Serial.print(F(","));
+  reportStatistics(&urHx711);
+  Serial.print(F(","));
+  reportStatistics(&lrHx711);
+  Serial.println();
   
-  value = llHx711.read();
-  Serial.print(value, 5);
-  Serial.print(",");
+#else
+
+  Serial.println(F("No normal code ready yet");
   
-  value = ulHx711.read();
-  Serial.print(value, 5);
-  Serial.print(",");
-  
-  value = urHx711.read();
-  Serial.print(value, 5);
-  Serial.print(",");
-  
-  value = lrHx711.read();
-  Serial.println(value, 5);
-  
+#endif
 }
+
+#ifdef CALIBRATE_SCALE
+/*
+ * Reports calibration statistics for the given Load Cell Amplifier.
+ * pHx = points to the corresponding Load Cell Amplifier controller.
+ */
+void reportStatistics(HX711 *pHx) {
+  float average;        // simple mean of values[]
+  float stddev;         // standard deviation from the mean.
+
+  // Collect a set of readings.
+  for (int i = 0; i < SAMPLES_PER_READING; ++i) {
+    values[i] = pHx->read();
+  }
+
+  // Calculate the Average (Mean)
+  average = 0.0f;
+  for (int i = 0; i < SAMPLES_PER_READING; ++i) {
+    average += values[i];
+  }
+  average /= (float) SAMPLES_PER_READING;
+
+  /*
+   * Calculate the standard deviation.
+   */
+
+  stddev = 0;
+  for (int i = 0; i < SAMPLES_PER_READING; ++i) {
+    stddev += (values[i] - average) * (values[i] - average);
+  }
+  stddev /= (float) SAMPLES_PER_READING;
+  stddev = (float) sqrt(stddev);
+
+  Serial.print(average, 1);
+  Serial.print(F(","));
+  Serial.print(stddev, 1);
+}
+#endif
+
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # CurieBLEWeightMonitor
 Arduino 101 (Curie) Sketch to continuously report my dog's weight using load sensors and Bluetooth Low Energy (BLE).
 ## Current Status
-The redesigned circuit is running. The Load Sensor holder design is finished. I'm editing the 3D printed Centering Guide, then need to write the Sketch that calculates weight and center of gravity. See ProjectDiary.odt for the progress on the project.
+The version 2 circuit is running. The 3D printed parts are finished and work great. I'm calibrating the scale, then need to write the Sketch that calculates weight and center of gravity. See ProjectDiary.odt for the progress on the project.
 ## Files
 - ProjectDiary.odt = day-to-day diary of the progress of the project. Useful to see how the project unfolded over time.
 - WeightScale.fzz = Fritzing circuit
 - BillOfMaterials.ods = parts list, with suppliers and prices.
-- MechanicalNotes.odp = diagrams of any complicated physical parts.
+- MechanicalNotes.odp = diagrams of any complicated physical parts. (needs to be updated for the Version 2 scale design)
 - ProjectMath.odp = mathematical background to the formulas in the code.
 - Calculations.ods = Spreadsheet of testing and design measurements.
 - loadsensorholder.FCStd = FreeCAD file for 3D printing hold-downs for the Load Sensors.
